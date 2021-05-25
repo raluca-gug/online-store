a1e19823515862fcebf574634ff6bdf8830911a0
Raluca <raluca.gug@accesa.eu>
2021-05-25 21:24:30 +0300

    feat: admin pannel

---
+++ src/services/service-requests.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceRequestsService {
+
+  constructor() { }
+
+  readLocalStorage(name: string) {
+    let storageData: { items?: any } = {};
+    storageData.items = [];
+    if (localStorage.getItem(name)) {
+      storageData.items = JSON.parse(localStorage.getItem(name)!).items;
+    }
+    return storageData.items;
+  }
+  writeLocalStorage(name: string, data: any) {
+    localStorage.setItem(name, JSON.stringify(data));
+  }
+  updateInvoices(invoice: any){
+    let edit=-1
+    let invoices: { items?: any } = {};
+    invoices.items=[];
+    invoices.items=this.readLocalStorage('invoiceData');
+    console.log('invoices in services', invoices);
+    invoices.items.forEach((el: any, i:number) => {
+      if(el.id==invoice.id) edit=i;
+    });
+    if(edit<0) invoices.items.push(invoice);
+    else invoices.items[edit]=invoice;
+    this.writeLocalStorage("invoiceData", invoices);
+  }
+  updateRequests(request: any){
+    let edit=-1;
+    let requests: { items?: any } = {};
+    requests.items=[];
+    requests.items=this.readLocalStorage('serviceRequests');
+    requests.items.forEach((el: any, i:number) => {
+      if(el.id==request.id) edit=i;
+    });
+    if(edit<0) requests.items.push(request);
+    else requests.items[edit]=request;
+    this.writeLocalStorage("serviceRequests", requests);
+  }
+  findInvoiceForRequest(request: any){
+    let invoices=this.readLocalStorage('invoiceData');
+    if (invoices) {
+      return invoices.filter((item:any)=> item.requestId==request.id)[0];
+    }
+    else return null;
+  }
+  
+}
