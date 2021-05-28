89c4667e62379f9dea9234b92f2117e35007a466
Raluca <raluca.gug@accesa.eu>
2021-05-28 12:49:56 +0300

    scroll directive with ternary op

--- src/app/shared-module/scroll-catcher/scroll-catcher.directive.ts
+++ src/app/shared-module/scroll-catcher/scroll-catcher.directive.ts
@@ -2,23 +2,22 @@ import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { Directive, HostListener, ViewChild } from '@angular/core';
 
 @Directive({
-  selector: '[appScrollCatcher]'
+  selector: '[appScrollCatcher]',
 })
 export class ScrollCatcherDirective {
-  mouse={x: 0, y:0};
-  previousY=0;
-  constructor() {
-    }
-  @HostListener('mousemove', ['$event']) getCoords($event: any){
-    this.mouse.x=$event.x;
-    this.mouse.y=$event.y;
-    console.log('mouse:', this.mouse)
+  mouse = { x: 0, y: 0 };
+  previousY = 0;
+  constructor() {}
+  @HostListener('mousemove', ['$event']) getCoords($event: any) {
+    this.mouse.x = $event.x;
+    this.mouse.y = $event.y;
+    console.log('mouse:', this.mouse);
   }
-  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any){
-    console.log(window.scrollY, "prev", this.previousY)
-    if(window.scrollY<this.previousY) this.mouse.y=this.mouse.y-this.previousY+window.scrollY;
-    else this.mouse.y=this.mouse.y+window.scrollY-this.previousY;
-    console.log('mouse:', this.mouse)
-    this.previousY=window.scrollY;
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any) {
+    console.log(window.scrollY, 'previ', this.previousY);
+    let direction = window.scrollY - this.previousY;
+    direction < 0 ? (this.mouse.y += direction) : (this.mouse.y += direction);
+    console.log('mouse:', this.mouse);
+    this.previousY = window.scrollY;
   }
 }
